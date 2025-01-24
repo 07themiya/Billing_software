283e0a1f8f1d1324ab4ad5b98caa976d0474c6c9
Themiya <thushanthemiya@gmail.com>
2025-01-24 23:51:43 +0530

    Footer
    Footer for the application

---
+++ billing-software/src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Created by: Thushan Chathuranga | Contact: thushanthemiya@gmail.com | +94 76 609 4226</p>
+    </footer>
+  );
+}
+
+export default Footer;
